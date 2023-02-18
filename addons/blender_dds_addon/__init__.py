c2f4c84a8b432e9b9273ebc3b1172443fa3baf0c
matyalatte <matyalatte@gmail.com>
2023-02-18 09:51:07 +0900

    update documents and workflows

--- addons/blender_dds_addon/__init__.py
+++ addons/blender_dds_addon/__init__.py
@@ -8,7 +8,7 @@ from .directx.texconv import unload_texconv
 bl_info = {
     'name': 'DDS textures',
     'author': 'Matyalatte',
-    'version': (0, 3, 0),
+    'version': (0, 3, 1),
     'blender': (2, 83, 20),
     'location': 'Image Editor > Sidebar > DDS Tab',
     'description': 'Import and export .dds files',
